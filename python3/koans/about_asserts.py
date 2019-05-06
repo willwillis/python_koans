8b05e790aa5bdda7c5b0489ff35c62fbbffcd2b6
Will Willis <will.willis@invesco.com>
2019-05-06 15:22:42 -0500

    brushing back up on Python after doing these Koans during 2010 PyTX : )

--- python3/koans/about_asserts.py
+++ python3/koans/about_asserts.py
@@ -20,19 +20,19 @@ class AboutAsserts(Koan):
         """
         Enlightenment may be more easily achieved with appropriate messages.
         """
-        self.assertTrue(False, "This should be True -- Please fix this")
+        self.assertTrue(True, "This should be True -- Please fix this")
 
     def test_fill_in_values(self):
         """
         Sometimes we will ask you to fill in the values
         """
-        self.assertEqual(__, 1 + 1)
+        self.assertEqual(2, 1 + 1)
 
     def test_assert_equality(self):
         """
         To understand reality, we must compare our expectations against reality.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
         self.assertTrue(expected_value == actual_value)
 
